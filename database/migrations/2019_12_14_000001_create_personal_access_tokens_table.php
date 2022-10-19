65c68ef4fba58d7fab7e5dd23f84289eb037491a
cumanakmamah <raja.parningot7@gmail.com>
2022-10-19 15:30:22 +0700

    migration

--- database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -13,13 +13,13 @@ class CreatePersonalAccessTokensTable extends Migration
      */
     public function up()
     {
-        Schema::create('personal_access_tokens', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
-            $table->morphs('tokenable');
-            $table->string('name');
-            $table->string('token', 64)->unique();
-            $table->text('abilities')->nullable();
-            $table->timestamp('last_used_at')->nullable();
+            $table->string('title');
+            $table->string('description');
+            $table->string('autor');
+            $table->string('publisher');
+            $table->date('date_of_issue');
             $table->timestamps();
         });
     }
